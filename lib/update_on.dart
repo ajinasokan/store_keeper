80565bf0d6ab72b65d9edcf4830cedfa30349f64
Ajin Asokan <ajin.panthayil@gmail.com>
2020-04-29 15:02:16 +0530

    fix exception on null mutation list

--- lib/update_on.dart
+++ lib/update_on.dart
@@ -5,18 +5,19 @@ import 'store_keeper.dart';
 class UpdateOn<T> extends StatelessWidget {
   final WidgetBuilder builder;
   final List<Object> mutations;
+  final Set<int> _mutCodes;
 
-  UpdateOn({this.builder, this.mutations});
+  UpdateOn({this.builder, this.mutations})
+      : _mutCodes = mutations?.map((i) => i.hashCode)?.toSet() ?? {};
 
   @override
   Widget build(BuildContext context) {
-    Set<int> mutCodes = mutations.map((i) => i.hashCode).toSet();
-
     Stream<int> stream;
-    if (mutations != null)
-      stream = StoreKeeper.events.where((e) => mutCodes.contains(e));
-    else
+    if (mutations != null) {
+      stream = StoreKeeper.events.where((e) => _mutCodes.contains(e));
+    } else {
       stream = StoreKeeper.getStreamOf(T);
+    }
 
     return StreamBuilder<int>(
       stream: stream,
